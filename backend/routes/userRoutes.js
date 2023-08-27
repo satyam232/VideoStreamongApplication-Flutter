8c617a4bd8911c3a35f2c50a5e3df07d81208b44
Satyam Rana <60211993+satyam232@users.noreply.github.com>
2023-08-27 17:20:04 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router(); // Use router instead of UserRoutes
 
-const UserController = require('../controller/userController'); // Correct the controller path
+const UserController = require('../controller/userController');
 
 // Define your routes using the router
 router.post('/movies', UserController.sendData);
  // Use the appropriate route and controller method
 
  router.get('/category/:id', UserController.fetchAllMoviesByCategoriesId);
-router.get('/movies', UserController.fetchData); // Use the appropriate route and controller method
+router.get('/movies', UserController.fetchData); 
 
 // Export the router
 module.exports = router;
